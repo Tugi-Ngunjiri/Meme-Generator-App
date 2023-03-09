70a0838b8e5bd2326c706e829b2cb08bd9e7ee7b
tugi <blueqwinn@gmail.com>
2023-03-09 22:38:48 +0300

    updated readme

--- my-app/src/Meme.js
+++ my-app/src/Meme.js
@@ -5,8 +5,14 @@ export default function Meme(props){
         <div className="meme-container">
              <nav className="navbar">
               <img  className="memeLOGO"src={props.memeLOGO} alt="memeLOGO"/>
-            <p>Meme Generator</p>
+            <p className="title">Meme Generator</p>
              </nav>
+
+             <div className="input-field" >
+                <input type="text" placeholder="Enter the first line"/>
+                  <input type="text" />
+
+             </div>
         </div>
     )
 }
